023149e134f6444a029463d55258f203bf894318
Jens Tinfors <jens.tinfors@gmail.com>
2014-01-03 21:35:59 +0100

    Adds setup script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
+function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
+function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
+function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
+
+# Ensure that we can actually, like, compile anything.
+if [[ ! "$(type -P gcc)" && "$OSTYPE" =~ ^darwin ]]; then
+  e_error "XCode or the Command Line Tools for XCode must be installed first."
+  exit 1
+fi
+
+if [[ "$OSTYPE" =~ linux-gnu ]]; then
+  [[ ! "$(type -P git)" ]] && sudo apt-get -qq install git
+fi
+
+if [[ ! -d ~/.vim ]]; then
+  e_header "Downloading dotfiles"
+  git clone --recursive git://github.com/jtinfors/dotvim.git ~/.vim
+  cd ~/.vim
+else
+  e_header "Updating dotfiles"
+  cd ~/.vim
+  git pull
+  git submodule update --init --recursive --quiet
+  cd -
+fi
+
+cd $HOME
+ln -s .vim/vimrc .vimrc
+ln -s .vim/gvimrc .gvimrc
+
